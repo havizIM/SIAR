b870fdd02b28d9cd524ad295ef02b2f5b125ab1b
havizIM <viz.ndinq@gmail.com>
2018-11-03 14:47:15 +0700

    Update Haviz

--- db_siar.sql
+++ db_siar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 08, 2018 at 09:46 AM
+-- Generation Time: Nov 03, 2018 at 08:45 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 5.6.35
 
@@ -21,30 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_siar`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `t_admin`
---
-
-CREATE TABLE `t_admin` (
-  `id_admin` varchar(5) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `level` varchar(20) NOT NULL,
-  `keterangan` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `t_admin`
---
-
-INSERT INTO `t_admin` (`id_admin`, `username`, `password`, `level`, `keterangan`) VALUES
-('AD001', 'admin', '40dc6c3b5c6595384395164908da32c18ae9dfc9', 'Admin', ''),
-('AD002', 'Yogi', 'd8ca2cd2609740765c3c01ffb55b4411a8330bad', 'Ketua RW', '005'),
-('AD003', 'Hanafi', '0a289766851125be33aded636ba8c6bfeaa58848', 'Ketua RT', '010/005'),
-('AD004', 'Zacky', 'c63f31e6ac0091b1aba38f5fa8ec7bedc7586bc1', 'Ketua RT', '001/005');
 
 -- --------------------------------------------------------
 
@@ -78,6 +54,7 @@ CREATE TABLE `t_anggota` (
 INSERT INTO `t_anggota` (`NIK`, `nama`, `jenis_kelamin`, `tempat_lahir`, `tgl_lahir`, `agama`, `pendidikan`, `pekerjaan`, `status_perkawinan`, `status_keluarga`, `kewarganegaraan`, `no_password`, `no_kitap`, `nama_ayah`, `nama_ibu`, `no_kk`) VALUES
 ('3173041010920016', 'Haviz Indra Maulana', 'Laki-laki', 'Jakarta', '1992-10-10', 'Islam', 'SLTA', 'Karyawan Swasta', 'Kawin', 'Kepala Keluarga', 'WNI', NULL, NULL, 'Muhammad Hanafi', 'Indrawati', '3173040708151022'),
 ('3173041706151005', 'Devan Dirgantara Putra', 'Laki-laki', 'Jakarta', '2015-06-17', 'Islam', 'Belum Sekolah', 'Belum Bekerja', 'Belum Kawin', 'Anak', 'WNI', NULL, NULL, 'Haviz Indra Maulana', 'Dian Ratna Sari', '3173040708151022'),
+('3173042301700008', 'Muhammad Hanafi', 'Laki-laki', 'Jakarta', '1970-01-23', 'Islam', 'SLTA/Sederajat', 'Wiraswasta', 'Cerai Hidup', 'Kepala Keluarga', 'WNI', NULL, NULL, 'H. Ilyas Arsadi', 'Hj. Suharsih', '3173041201095931'),
 ('3173044712160004', 'Kalyssa Innara Putri', 'Perempuan', 'Jakarta', '2016-12-07', 'Islam', 'Belum Sekolah', 'Belum Bekerja', 'Belum Kawin', 'Anak', 'WNI', NULL, NULL, 'Haviz Indra Maulana', 'Dian Ratna Sari', '3173040708151022'),
 ('3173046711950003', 'Dian Ratna Sari', 'Perempuan', 'Jakarta', '1995-11-27', 'Islam', 'SLTA', 'Karyawan Swasta', 'Kawin', 'Istri', 'WNI', NULL, NULL, 'Sudji', 'Sri Jueni', '3173040708151022');
 
@@ -92,7 +69,9 @@ CREATE TABLE `t_dokumen` (
   `keterangan` varchar(100) NOT NULL,
   `foto_dokumen` text NOT NULL,
   `no_pengajuan` varchar(13) NOT NULL,
-  `tgl_upload` date NOT NULL
+  `tgl_upload` date NOT NULL,
+  `diambil_oleh` varchar(50) NOT NULL,
+  `tgl_pengambilan` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -103,17 +82,24 @@ CREATE TABLE `t_dokumen` (
 
 CREATE TABLE `t_keluarga` (
   `no_kk` varchar(16) NOT NULL,
+  `tgl_kk` date NOT NULL,
   `alamat` varchar(100) NOT NULL,
-  `rt/rw` varchar(10) NOT NULL,
-  `tgl_kk` date NOT NULL
+  `kode_pos` varchar(10) NOT NULL,
+  `provinsi` varchar(30) NOT NULL,
+  `kotamadya` varchar(30) NOT NULL,
+  `kecamatan` varchar(30) NOT NULL,
+  `kelurahan` varchar(30) NOT NULL,
+  `rtrw` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `t_keluarga`
 --
 
-INSERT INTO `t_keluarga` (`no_kk`, `alamat`, `rt/rw`, `tgl_kk`) VALUES
-('3173040708151022', 'Gg. Vanilli No.19f', '010/005', '2017-03-30');
+INSERT INTO `t_keluarga` (`no_kk`, `tgl_kk`, `alamat`, `kode_pos`, `provinsi`, `kotamadya`, `kecamatan`, `kelurahan`, `rtrw`) VALUES
+('3173040708151022', '2017-03-30', 'Gg. Vanilli No.19f', '11210', 'DKI Jakarta', 'Jakarta Barat', 'Tambora', 'Tanah Sereal', '010/005'),
+('3173041201095931', '2018-11-03', 'Gg. Vanilli No. 19f', '11210', 'DKI Jakarta', 'Jakarta Barat', 'Tambora', 'Tanah Sereal', '010/005'),
+('3811895719759314', '2018-11-03', 'Gg. Vanilli no. 19f', '11210', 'DKI Jakarta', 'Jakarta Barat', 'Tambora', 'Tanah Sereal', '011/005');
 
 -- --------------------------------------------------------
 
@@ -127,6 +113,15 @@ CREATE TABLE `t_pelengkap` (
   `keterangan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_pelengkap`
+--
+
+INSERT INTO `t_pelengkap` (`no_pengajuan`, `foto_dokumen`, `keterangan`) VALUES
+('PJ00000000002', 'dok_PJ00000000002.jpg', 'KK'),
+('PJ00000000002', 'dok_PJ000000000021.jpg', 'blabla'),
+('PJ00000000003', 'dok_PJ00000000003.jpg', 'blabla');
+
 -- --------------------------------------------------------
 
 --
@@ -135,13 +130,29 @@ CREATE TABLE `t_pelengkap` (
 
 CREATE TABLE `t_pengajuan` (
   `no_pengajuan` varchar(13) NOT NULL,
-  `tgl_pengajuan` date NOT NULL,
   `no_kk` varchar(16) NOT NULL,
-  `keperluan` varchar(30) NOT NULL,
-  `status_pengajuan` enum('Proses','Menunggu','Selesai','') NOT NULL,
+  `tgl_pengajuan` date NOT NULL,
   `NIK` varchar(16) NOT NULL,
-  `nama_pengajuan` varchar(100) NOT NULL
+  `nama_pengajuan` varchar(100) NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan','','') NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `tempat_lahir` varchar(15) NOT NULL,
+  `pekerjaan` varchar(20) NOT NULL,
+  `kewarganegaraan` enum('WNI','WNA','','') NOT NULL,
+  `pendidikan` varchar(20) NOT NULL,
+  `agama` enum('Islam','Kristen','Katholik','Budha','Hindu','Lainnya') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `keperluan` varchar(30) NOT NULL,
+  `status_pengajuan` enum('Proses','Menunggu','Selesai','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_pengajuan`
+--
+
+INSERT INTO `t_pengajuan` (`no_pengajuan`, `no_kk`, `tgl_pengajuan`, `NIK`, `nama_pengajuan`, `jenis_kelamin`, `tgl_lahir`, `tempat_lahir`, `pekerjaan`, `kewarganegaraan`, `pendidikan`, `agama`, `alamat`, `keperluan`, `status_pengajuan`) VALUES
+('PJ00000000002', '3173040708151022', '2018-11-01', '3173046711950003', 'Dian Ratna Sari', 'Perempuan', '1995-11-27', 'Jakarta', 'Karyawan Swasta', 'WNI', 'SLTA/Sederajat', 'Islam', 'Jl. Gg Vanilli no 19f Jakarta', 'Membuat KTP', 'Proses'),
+('PJ00000000003', '3173040708151022', '2018-11-03', '3301029919192000', 'yugi setiawan', 'Perempuan', '2019-01-14', 'Jakarta', 'nganggur', 'WNI', 'S!', 'Islam', 'Jakarta', 'Surat Kehilangan', 'Proses');
 
 -- --------------------------------------------------------
 
@@ -152,25 +163,22 @@ CREATE TABLE `t_pengajuan` (
 CREATE TABLE `t_user` (
   `no_kk` varchar(16) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `level` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `t_user`
 --
 
-INSERT INTO `t_user` (`no_kk`, `email`, `password`) VALUES
-('3173040708151022', 'viz.ndinq@gmail.com', '9cb559c8b811362e7657090091052e8542176009');
+INSERT INTO `t_user` (`no_kk`, `email`, `password`, `level`) VALUES
+('3811895719759314', 'coba@gmail.com', '34f70892f40cd3b2a340769c070c4f1a02335d87', 'RW'),
+('3173041201095931', 'hanafi@gmail.com', '0a289766851125be33aded636ba8c6bfeaa58848', 'RT'),
+('3173040708151022', 'viz.ndinq@gmail.com', '9cb559c8b811362e7657090091052e8542176009', 'Warga');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `t_admin`
---
-ALTER TABLE `t_admin`
-  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indexes for table `t_anggota`
@@ -232,7 +240,7 @@ ALTER TABLE `t_dokumen`
 -- Constraints for table `t_pelengkap`
 --
 ALTER TABLE `t_pelengkap`
-  ADD CONSTRAINT `t_pelengkap_ibfk_1` FOREIGN KEY (`no_pengajuan`) REFERENCES `t_pengajuan` (`no_pengajuan`);
+  ADD CONSTRAINT `t_pelengkap_ibfk_1` FOREIGN KEY (`no_pengajuan`) REFERENCES `t_pengajuan` (`no_pengajuan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `t_pengajuan`
